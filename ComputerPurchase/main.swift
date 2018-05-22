f075e0be964cc0251d143d8803e1e09b4b21c541
Yuen, Geoff <gyuen@lcs-mba3098.lcs.on.ca>
2018-05-22 16:04:26 -0400

    done

--- ComputerPurchase/main.swift
+++ ComputerPurchase/main.swift
@@ -2,23 +2,35 @@
 //  main.swift
 //  ComputerPurchase
 //
-
 import Foundation
 
 // INPUT
-// Global variable, tracks how many computers we are going to collect specs for
-var countOfExpectedSpecsThatWillBeProvided = 3
 
-// Write a loop to actually determine how many computer specs the user will provide
-// e.g.: write the rest of the INPUT section
-
+var givenSpecs = 0
+while true {
+    
+    print("How many specs will be provided?")
+    guard let input = readLine() else{
+        continue
+    }
+    
+    guard let intInput = Int(input) else {
+        continue
+    }
+    
+    if intInput < 0 || intInput > 10 {
+        continue
+    }
+    
+    givenSpecs = intInput
+    break
+}
 
 // PROCESS & OUTPUT
-// Implement the primary logic of the problem here
-// Some output may be given here if you desire
-
+var bestComputerSpec = 0
+var bestComputerName = ""
 // Collect the list of computer specs here
-for counter in 1...countOfExpectedSpecsThatWillBeProvided {
+for counter in 1...givenSpecs {
     
     // Ask user for the specs for a given computer
     print("Spec \(counter)?")
@@ -30,24 +42,21 @@ for counter in 1...countOfExpectedSpecsThatWillBeProvided {
         continue
     }
     
-    // What was provided?
-    print("The given input was: \(givenInput)")
+    let splitInput = givenInput.split(separator: " ")
+    let computerName = String(splitInput[0])
+    let computerRAM = Int(splitInput[1])
+    let computerCPU = Int(splitInput[2])
+    let computerDiskSpace = Int(splitInput[3])
     
-    // NOTE:
-    //
-    // Some example code that may be useful
-    let exampleInput = "SuperFastComputer 1000 50 75"
-    print("The example input is: \(exampleInput)")
-    let exampleInputPieces = exampleInput.split(separator: " ")
-    let computerName = exampleInputPieces[0]
-    let computerRAM = exampleInputPieces[1]
-    let computerCPU = exampleInputPieces[2]
-    let computerDiskSpace = exampleInputPieces[3]
-    print("Computer name is: \(computerName)")
-    print("Computer RAM amount is: \(computerRAM)")
-    print("Computer CPU speed is: \(computerCPU)")
-    print("Computer disk space is: \(computerDiskSpace)")
+    let specResult = 2 * computerRAM! + 3 * computerCPU! + computerDiskSpace!
     
-    // Implement the rest of your logic here...
+    
+    if specResult > bestComputerSpec {
+        bestComputerSpec = specResult
+        bestComputerName = computerName
+    }
     
 }
+
+
+print("The best computer for you is \(bestComputerName)")
